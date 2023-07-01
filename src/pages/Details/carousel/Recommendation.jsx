77ab3c943e3b90bcdec3e316c56e688823eebed7
Aman Upadhyay <105794639+upaaman@users.noreply.github.com>
2023-07-02 00:09:02 +0530

    Code Completed

---
+++ src/pages/Details/carousel/Recommendation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+const Recommendation = ({ mediaType, id }) => {
+    const { data, loading, error } = useFetch(
+        `/${mediaType}/${id}/recommendations`
+    );
+    return (
+        <div>
+            {
+                data?.total_results > 0&&
+                <Carousel
+                title="Recommendations"
+                data={data?.results}
+                loading={loading}
+                endpoint={mediaType} />
+            }
+          
+        </div>
+
+    );
+};
+
+export default Recommendation;
